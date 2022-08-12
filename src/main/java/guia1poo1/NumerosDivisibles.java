81d0cfaac63d447c04d74f7edaabb99e5b862e2d
CARLA MARIA ARTIGA NAVARRETE <202000791@ujmd.edu.sv>
2022-08-12 10:07:26 -0600

    Se agrego nueva clase java NumerosDivisibles

---
+++ src/main/java/guia1poo1/NumerosDivisibles.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guia1poo1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class NumerosDivisibles {
+    public static void main(String[] args) {
+        
+    }
+}
