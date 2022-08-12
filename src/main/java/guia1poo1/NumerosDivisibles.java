60700a73ff3f6ecb5b22e30ef14e2de4d0b6d0ca
CARLA MARIA ARTIGA NAVARRETE <202000791@ujmd.edu.sv>
2022-08-12 10:33:03 -0600

    Se agregaron nuevas lineas de codigo a la clase NumerosDivisbles

--- src/main/java/guia1poo1/NumerosDivisibles.java
+++ src/main/java/guia1poo1/NumerosDivisibles.java
@@ -8,8 +8,34 @@ package guia1poo1;
  *
  * @author LENOVO
  */
+import java.util.*;
 public class NumerosDivisibles {
     public static void main(String[] args) {
+        //variables para operaciones
+        int numero1;
+        int numero2;
+        
+        
+        //variable scanner
+        Scanner reader  = new Scanner(System.in);
+        
+        //Titulo del programa
+        System.out.println("*** Programa que verifica si son numeros divisibles entre si ***");
+        
+        //Pedir al usuario dos numeros
+        
+        System.out.print("Escriba el primero numero: ");
+        numero1 = reader.nextInt();
+        System.out.print("Escriba el segundo numero: ");
+        numero2 = reader.nextInt();
+        
+        
+       if(numero1%numero2==0){
+           System.out.println(numero1 + " y " + numero2 + " son divisibles ");
+           
+       }
+       System.exit(0);
+        
         
     }
 }
