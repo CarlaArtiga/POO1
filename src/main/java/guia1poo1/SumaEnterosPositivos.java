50fe12d8bbcb759c328b93f1f29432c8ec2f1c20
CARLA MARIA ARTIGA NAVARRETE <202000791@ujmd.edu.sv>
2022-08-12 10:44:56 -0600

    Nuevas lineas de codigo en la clase SumaEnterosPositivos

--- src/main/java/guia1poo1/SumaEnterosPositivos.java
+++ src/main/java/guia1poo1/SumaEnterosPositivos.java
@@ -8,9 +8,48 @@ package guia1poo1;
  *
  * @author LENOVO
  */
+import javax.swing.JOptionPane;
 public class SumaEnterosPositivos {
     public static void main(String[] args) {
+        //Declaramos estas variables de tipo string debido a que el cuadro de dialogo no admite numeros
+        //Por lo que es necesario convertir de string a entero
+        String primernumero;
+        String segundonumero;
         
+        //Variables que nos sirven para hacer una operacion
+        int numero1;
+        int numero2;
+        int suma;
+        
+        primernumero = JOptionPane.showInputDialog("Digite el primero numero");
+        segundonumero = JOptionPane.showInputDialog("Digite el segundo numero");
+        
+        //Conversion de tipo de datos string a enteros
+        numero1 = Integer.parseInt(primernumero);
+        numero2 = Integer.parseInt(segundonumero);
+        
+        
+        //Operacion
+        suma = numero1 + numero2;
+        
+        //El argumento resultados es el titulo del cuadro de dialogo
+        //El cuarto argumento es un estilo del cuadro de dialogo
+        //El operador suma es para concatenar
+        if(numero1<=0 ||numero2<=0)
+        {
+            JOptionPane.showMessageDialog(null,"No se admiten numero negativos");
+        
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"La suma es: "+suma,"Resultado",JOptionPane.PLAIN_MESSAGE);
+        }
+        
+        
+        
+        
+        
+        System.exit(0);
+       
     
     
     }
