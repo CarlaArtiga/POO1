ae6ccc02c27f5378ce60a467987ea652ca5f8660
CARLA MARIA ARTIGA NAVARRETE <202000791@ujmd.edu.sv>
2022-08-12 09:58:27 -0600

    Se agregaron nuevas filas de codigo

--- src/main/java/guia1poo1/Comparación.java
+++ src/main/java/guia1poo1/Comparación.java
@@ -8,8 +8,49 @@ package guia1poo1;
  *
  * @author LENOVO
  */
+import javax.swing.JOptionPane;
 public class Comparación {
-    public static void main(String[] args{
-    
+    public static void main(String[] args) {
+        
+        //variables
+        String primernumero;
+        String segundonumero;
+        String resultado;
+        
+        int numero1;
+        int numero2;
+        
+        primernumero = JOptionPane.showInputDialog("Escriba el primero numero");
+        segundonumero = JOptionPane.showInputDialog("Escribra el segundo numeri");
+        
+        numero1 = Integer.parseInt(primernumero);
+        numero2 = Integer.parseInt(segundonumero);
+        
+        resultado = "";
+        
+        if(numero1 == numero2){
+            resultado = resultado + numero1 + " == " + numero2;
+            
+        }
+        if(numero1!=numero2){
+            resultado = resultado + numero1 + " != " + numero2;
+            
+        }
+        if(numero1<numero2){
+            resultado = resultado + numero1 + " < " + numero2;
+        }
+        if(numero1>numero2){
+            resultado = resultado + numero1 + " > " + numero2;
+        }
+        if(numero1<=numero2){
+            resultado = resultado + numero1 + " <= " + numero2;
+        }
+        if(numero1>=numero2){
+            resultado = resultado + numero1 + " >= " + numero2;
+        }
+        JOptionPane.showConfirmDialog(null, resultado,"Resultado de la comparacion",
+                                      JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+              
     }
 }
